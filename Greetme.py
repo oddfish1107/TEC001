a98f61faea23bd0dc259e103b66e97f17afcfce0
oddfish1107 <164762843+oddfish1107@users.noreply.github.com>
2026-01-08 09:22:30 +0700

    Add files via upload

---
+++ Greetme.py
@@ -0,0 +1,8 @@
+import math
+name = input("What is your name? ")
+print("Hello,"+ name,"!")
+
+circleradius = float(input("What is the radius of the circle? "))
+circumference = 2 * math.pi * circleradius
+print("The circumference of the circle is ",math.floor(circumference))
+
